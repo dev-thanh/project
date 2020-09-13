68ce81783ae3a8188a150a8fc3fe62748fb5601a
Thanh <47233653+dev-thanh@users.noreply.github.com>
2020-09-13 17:29:05 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -28,9 +28,9 @@ Route::group(['middleware' => 'admin','prefix' => 'admin'], function () {
     //Route::post( '/user/{data}', 'UserController@postData' );
 });
 
-// Route::get('test-queue','TestController@test_queue')->name('test-queue');
-// Route::get('image/index', 'ImageController@index');
-// Route::post('image/upload', 'ImageController@upload');
+Route::get('test-queue','TestController@test_queue')->name('test-queue');
+Route::get('image/index', 'ImageController@index');
+Route::post('image/upload', 'ImageController@upload');
 // Route::get('/mail', 'ImageController@mail');
 // Route::post('/posts', 'ImageController@store');
 // Route::get('git-test', 'ImageController@git_Test')->name('git-test');
